b45350274b055a1d7c7f7b1fe449edf60a071fe9
masthan shareef <shareefshaik3162@gmail>
2026-05-01 11:44:06 +0530

    removed backend and this repo is only for frontend

--- TradeManagement_backend/src/main/java/com/example/TradeManagement_backend/utils/ResultStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.TradeManagement_backend.utils;
-
-public enum ResultStatus {
-	PROFIT,LOSS
-
-}
--- TradeManagement_backend/src/main/java/com/example/TradeManagement_backend/utils/TransactionType.java
+++
@@ -1,5 +0,0 @@
-package com.example.TradeManagement_backend.utils;
-
-public enum TransactionType {
-ADD,WITHDRAW
-}
